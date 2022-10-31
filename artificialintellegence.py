467d9108f2b9d1c6fb2f573e94b1b3274ad724c7
Vedanth-Powar <105548301+Vedanth-Powar@users.noreply.github.com>
2022-10-31 10:59:10 +0530

    Create artificialintellegence.py

---
+++ artificialintellegence.py
@@ -0,0 +1,19 @@
+model = Sequential()
+model.add(Conv2D(32, (2, 2), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (2, 2)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (2, 2)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
